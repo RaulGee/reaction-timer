ff69e9364c7402be14a851d08f49318942c94794
Raul Guirado <raul.guirado95@gmail.com>
2021-03-03 23:03:18 -0800

    initial commit

---
+++ src/components/Results.vue
@@ -0,0 +1,28 @@
+<template>
+  <p>Reaction Time: {{ score }}ms</p>
+  <p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+  props: ['score'],
+  data() {
+    return {
+      rank: null
+    }
+  },
+  mounted() {
+    if (this.score < 250) {
+      this.rank = 'Faster than the Flash'
+    } else if (this.score < 400) {
+      this.rank = 'Cat like reflexes'
+    } else {
+      this.rank = `I didn't know snails could play this game...`
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
